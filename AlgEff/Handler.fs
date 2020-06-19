e74d8d5fb666940e32bacf8d16edc226a3966c10
Brian Berns <brianberns@gmail.com>
2020-06-19 14:06:33 -0400

    Added type constraint.

--- AlgEff/Handler.fs
+++ AlgEff/Handler.fs
@@ -1,6 +1,6 @@
 ﻿namespace AlgEff
 
-type EffectHandler<'state, 'effect, 'next> =
+type EffectHandler<'state, 'effect, 'next when 'effect :> Effect<'next>> =
     abstract member Start : 'state
     abstract member Step : 'state * 'effect -> ('state * 'next)
     abstract member Finish : 'state -> 'state
