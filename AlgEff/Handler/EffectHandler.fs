dac0e70b0111b7671bd52a3514dce4de7af99d67
Brian Berns <brianberns@gmail.com>
2020-06-24 09:49:46 -0400

    Minor refactor.

--- AlgEff/Handler/EffectHandler.fs
+++ AlgEff/Handler/EffectHandler.fs
@@ -8,9 +8,10 @@ type EffectHandler<'next, 'state, 'finish> =
         /// Handler's initial state.
         Start : 'state
 
-        /// Handles a single effect in the given state, answering the updated state
-        /// and the next effect.
-        Step : ('state * Effect<'next>) -> Option<'state * 'next>
+        /// Attempts to handle a single effect in the given state,
+        /// answering the updated state and the next effect if
+        /// successful.
+        TryStep : ('state * Effect<'next>) -> Option<'state * 'next>
 
         /// Transforms the handler's final state.
         Finish : 'state -> 'finish
@@ -18,32 +19,28 @@ type EffectHandler<'next, 'state, 'finish> =
 
 module EffectHandler =
 
-    /// Uses reflection to convert the given step function for use
-    /// in an effect handler.
-    let toStep<'effect, 'next, 'state when 'effect :> Effect<'next>>
-        (step : ('state * 'effect) -> ('state * 'next)) =
-
-        fun (state : 'state, effect : Effect<'next>) ->
-            match effect with
-                | :? 'effect as typedEffect ->
-                    step (state, typedEffect) |> Some
-                | _ -> None
-
     /// Creates an effect handler.
     let create start step finish =
         {
             Start = start
-            Step = step
+            TryStep = step
             Finish = finish
         }
 
     /// Creates an effect handler by adapting the given step function.
     let adapt state step finish =
-        create state (toStep step) finish
+
+        let tryStep step (state, effect : Effect<'next>) =
+            match effect with
+                | :? 'effect as typedEffect ->
+                    step (state, typedEffect) |> Some
+                | _ -> None
+
+        create state (tryStep step) finish
 
     /// Maps a function over an effect handler.
     let map f handler =
-        create handler.Start handler.Step (handler.Finish >> f)
+        create handler.Start handler.TryStep (handler.Finish >> f)
 
     /// Runs the given program using the given handler.
     let run program handler =
@@ -51,7 +48,7 @@ module EffectHandler =
         /// Runs a single step in the program.
         let rec loop state = function
             | Free effect ->
-                match handler.Step(state, effect) with
+                match handler.TryStep(state, effect) with
                     | Some (state', next) -> loop state' next
                     | None -> failwithf "Unhandled effect: %A" effect
             | Pure result ->
@@ -66,11 +63,11 @@ module EffectHandler =
         let start = handler1.Start, handler2.Start
 
         let step ((state1, state2), effect) =
-            handler1.Step(state1, effect)
+            handler1.TryStep(state1, effect)
                 |> Option.map (fun (state1', result) ->
                     (state1', state2), result)
                 |> Option.orElseWith (fun () ->
-                    handler2.Step(state2, effect)
+                    handler2.TryStep(state2, effect)
                         |> Option.map (fun (state2', result) ->
                             (state1, state2'), result))
 
@@ -96,4 +93,5 @@ module EffectHandler =
 
 /// A concrete context that satisfies an effect's context
 /// requirement.
+[<AbstractClass>]
 type ConcreteContext<'res>() = class end
