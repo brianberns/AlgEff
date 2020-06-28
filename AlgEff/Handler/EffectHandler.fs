58906ad7c44ebe32a2a96a7712bbe39ca645041a
Brian Berns <brianberns@gmail.com>
2020-06-28 02:01:42 -0400

    Minor cleanup.

--- AlgEff/Handler/EffectHandler.fs
+++ AlgEff/Handler/EffectHandler.fs
@@ -12,7 +12,11 @@ type EffectHandler<'ctx, 'res, 'state, 'finish>() =
     /// Attempts to handle a single effect in the given state,
     /// answering the updated state and the next effect if
     /// successful.
-    abstract member TryStep<'outState> : 'state * Effect<EffectChain<'ctx, 'res>> * EffectHandlerCont<'ctx, 'res, 'state, 'outState> -> Option<'outState * 'res>
+    abstract member TryStep<'outState> :
+        'state
+            * Effect<EffectChain<'ctx, 'res>>
+            * EffectHandlerCont<'ctx, 'res, 'state, 'outState>
+            -> Option<'outState * 'res>
 
     /// Transforms the handler's final state.
     abstract member Finish : 'state -> 'finish
@@ -22,7 +26,7 @@ type EffectHandler<'ctx, 'res, 'state, 'finish>() =
 
         /// Runs a single step in the program.
         let rec loop state = function
-            | Free (effect : Effect<EffectChain<'ctx, 'res>>) ->
+            | Free effect ->
                 match this.TryStep(state, effect, loop) with
                     | Some (state', result) -> state', result
                     | None -> failwithf "Unhandled effect: %A" effect
@@ -32,15 +36,18 @@ type EffectHandler<'ctx, 'res, 'state, 'finish>() =
         let state, result = loop this.Start program
         result, this.Finish(state)
 
-and EffectHandlerCont<'ctx, 'res, 'state, 'outState> = 'state -> EffectChain<'ctx, 'res> -> ('outState * 'res)
+and EffectHandlerCont<'ctx, 'res, 'state, 'outState> =
+    'state -> EffectChain<'ctx, 'res> -> ('outState * 'res)
 
 /// Combines two effect handlers.
-type CombinedEffectHandler<'ctx, 'res, 'state1, 'finish1, 'state2, 'finish2>(handler1 : EffectHandler<'ctx, 'res, 'state1, 'finish1>, handler2 : EffectHandler<'ctx, 'res, 'state2, 'finish2>) =
+type CombinedEffectHandler<'ctx, 'res, 'state1, 'finish1, 'state2, 'finish2>
+    (handler1 : EffectHandler<'ctx, 'res, 'state1, 'finish1>,
+    handler2 : EffectHandler<'ctx, 'res, 'state2, 'finish2>) =
     inherit EffectHandler<'ctx, 'res, 'state1 * 'state2, 'finish1 * 'finish2>()
 
     override __.Start = handler1.Start, handler2.Start
 
-    override __.TryStep<'outState>((state1, state2), effect, cont : EffectHandlerCont<_, _, 'state1 * 'state2, 'outState>) =
+    override __.TryStep<'outState>((state1, state2), effect, cont : EffectHandlerCont<_, _, _, 'outState>) =
         let cont1 state1' chain =
             cont (state1', state2) chain
         let cont2 state2' chain =
