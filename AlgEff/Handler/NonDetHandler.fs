51e99f379de80b5b73c4f41aefd0f279e3b28b51
Brian Berns <brianberns@gmail.com>
2020-06-28 08:45:49 -0400

    Tweak to previous.

--- AlgEff/Handler/NonDetHandler.fs
+++ AlgEff/Handler/NonDetHandler.fs
@@ -2,7 +2,7 @@
 
 open AlgEff.Effect
 
-type Dummy = Dummy   // why doesn't unit work?
+type Dummy = Dummy   // unit doesn't work
 
 type PickTrue<'ctx, 'res when 'ctx :> NonDetContext and 'ctx :> ConcreteContext<'res>>(context : 'ctx) =
     inherit EffectHandler<'ctx, 'res, Dummy, Dummy>()
@@ -19,25 +19,25 @@ type PickTrue<'ctx, 'res when 'ctx :> NonDetContext and 'ctx :> ConcreteContext<
                     |> Some
             | _ -> None
 
-    override __.Finish(dummy) = dummy
+    override __.Finish(Dummy) = Dummy
 
-    type PickMax<'ctx, 'res when 'ctx :> NonDetContext and 'ctx :> ConcreteContext<'res> and 'res : comparison>(context : 'ctx) =
-        inherit EffectHandler<'ctx, 'res, Dummy, Dummy>()
+type PickMax<'ctx, 'res when 'ctx :> NonDetContext and 'ctx :> ConcreteContext<'res> and 'res : comparison>(context : 'ctx) =
+    inherit EffectHandler<'ctx, 'res, Dummy, Dummy>()
 
-        override __.Start = Dummy
+    override __.Start = Dummy
 
-        override __.TryStep(_, effect, cont) =
-            match effect with
-                | :? NonDetEffect<EffectChain<'ctx, 'res>> as nonDetEff ->
-                    match nonDetEff.Case with
-                        | Decide eff ->
-                            let outStateT, resT = eff.Cont(true) |> cont Dummy
-                            let outStateF, resF = eff.Cont(false) |> cont Dummy
-                            if resT > resF then
-                                outStateT, resT
-                            else
-                                outStateF, resF
-                        |> Some
-                | _ -> None
+    override __.TryStep(_, effect, cont) =
+        match effect with
+            | :? NonDetEffect<EffectChain<'ctx, 'res>> as nonDetEff ->
+                match nonDetEff.Case with
+                    | Decide eff ->
+                        let outStateT, resT = eff.Cont(true) |> cont Dummy
+                        let outStateF, resF = eff.Cont(false) |> cont Dummy
+                        if resT > resF then
+                            outStateT, resT
+                        else
+                            outStateF, resF
+                    |> Some
+            | _ -> None
 
-        override __.Finish(dummy) = dummy
+    override __.Finish(Dummy) = Dummy
